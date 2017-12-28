b8abaf04fdbe0493bb1462da673d0a5e502e5376
Grégory <gregory.bouloc@gmail.com>
2017-12-28 09:41:57 +0100

    récupération et écriture des données météo

--- js/app.js
+++ js/app.js
@@ -1,74 +1,43 @@
 (function main(){
-var xhr=createXHR();	
-$('#mybtn').on('click',function(){
-	//getJson();
-	getJqAjax();
-})
-function getJqAjax(){
-	var mylat = $('#mylat').val();
-	var mylng = $('#mylng').val();
-	$.ajax({
-		url:"https://www.prevision-meteo.ch/services/json/lat="+ mylat +"lng="+mylng+"",
-		datatype:'json',
-		success: function(data){
-			console.log("success");
-			console.log(data.fcst_day_0.condition)
-		},
-		error: function(){
-			console.log("no way, i'm fucked");
-		}
-	})
-}
-function getJson(){
-	var mylat = $('#mylat').val();
-	var mylng = $('#mylng').val();
-
-	var myurl = "https://www.prevision-meteo.ch/services/json/lat="+ mylat +"lng="+mylng;
-	//var myurl = "http://www.geobi.fr/dev/googleplace/places.php?lat="+ mylat +"&lon="+mylng+"&type=pharmacy&radius=10000";
-	//"http://www.geobi.fr/dev/googleplace/places.php?lat=44.334862&lon=2.435056&type=pharmacy&radius=10000"
-	var objJSON;
 	
-	xhr.open("GET", myurl,true);
+	$("#selec").on("click", function(){
 
-	xhr.onreadystatechange=function()
-	{
-				
-		if (xhr.readyState == 4) 
-		{
-			if (xhr.status != 404) 
-			{
-				var maMeteo=eval("(" + xhr.responseText + ")");
-				var myDiv = $('#meteo');
-				var contenu ="";
-				//var contenu = maMeteo.
-				var myCondition = maMeteo.fcst_day_0.condition;
-				alert(mycondition);
-			}
-		}
-	}
-	xhr.send(null);
-}
+		// Variable globale 
+		//******************************************************************************************
+		var city = $("#ville").val();
+		var keyApi = "5fe735476d87c79b7f242f8f480d72a0";
+		var lien = 'https://api.openweathermap.org/data/2.5/weather?q=' + city ;
 
-function createXHR() 
-{
-    var request = false;
-        try {
-            request = new ActiveXObject('Msxml2.XMLHTTP');
-        }
-        catch (err2) {
-            try {
-                request = new ActiveXObject('Microsoft.XMLHTTP');
-            }
-            catch (err3) {
-		try {
-			request = new XMLHttpRequest();
-		}
-		catch (err1) 
-		{
-			request = false;
-		}
-            }
-        }
-    return request;
-}
+		// Récupère le fichier JSON qui se trouve sur le lien.
+		//*******************************************************************************************
+		$.getJSON(lien +',fr&lang=fr&units=metric&appid=' + keyApi)
+  		.then(function (data) {
+
+  			// Récupération des données spécifique 
+  			//*****************************************************
+  			var name = data.name;
+     		var temp = data.main.temp_min;
+     		var humidite = data.main.humidity;
+     		var lattitude = data.coord.lat;// coordonnée
+     		var longitude = data.coord.lon;
+     		var coord = data.coord;
+     		// vitesse vent
+     		var vent = data.wind.speed;
+     		// pression
+     		var pression = data.main.pressure; 
+     		// affihcage des donées 
+
+     		$('.temperature').append(temp);
+     		console.log(temp + "°C");
+     		console.log(humidite);
+     		console.log(name);
+     		console.log(lattitude);
+     		console.log(longitude);
+     		console.log(vent);
+     		console.log(pression);
+     		console.log(coord);
+		});
+
+	});
+
 })();
