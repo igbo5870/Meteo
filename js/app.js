b5d62714de5f5e751ddb55d2fdeb5b5f404324ad
simplonfl <fabrice-laroche81@live.fr>
2017-12-28 09:43:57 +0100

    Ajout et récupération données météorologiques du JSON dans console

--- js/app.js
+++ js/app.js
@@ -1,74 +1,36 @@
 (function main(){
-var xhr=createXHR();	
-$('#mybtn').on('click',function(){
-	//getJson();
-	getJqAjax();
-})
-function getJqAjax(){
-	var mylat = $('#mylat').val();
-	var mylng = $('#mylng').val();
-	$.ajax({
-		url:"https://www.prevision-meteo.ch/services/json/lat="+ mylat +"lng="+mylng+"",
-		datatype:'json',
-		success: function(data){
-			console.log("success");
-			console.log(data.fcst_day_0.condition)
-		},
-		error: function(){
-			console.log("no way, i'm fucked");
-		}
-	})
-}
-function getJson(){
-	var mylat = $('#mylat').val();
-	var mylng = $('#mylng').val();
 
-	var myurl = "https://www.prevision-meteo.ch/services/json/lat="+ mylat +"lng="+mylng;
-	//var myurl = "http://www.geobi.fr/dev/googleplace/places.php?lat="+ mylat +"&lon="+mylng+"&type=pharmacy&radius=10000";
-	//"http://www.geobi.fr/dev/googleplace/places.php?lat=44.334862&lon=2.435056&type=pharmacy&radius=10000"
-	var objJSON;
-	
-	xhr.open("GET", myurl,true);
+		$('#selec').on('click', function(){
 
-	xhr.onreadystatechange=function()
-	{
-				
-		if (xhr.readyState == 4) 
-		{
-			if (xhr.status != 404) 
-			{
-				var maMeteo=eval("(" + xhr.responseText + ")");
-				var myDiv = $('#meteo');
-				var contenu ="";
-				//var contenu = maMeteo.
-				var myCondition = maMeteo.fcst_day_0.condition;
-				alert(mycondition);
-			}
-		}
-	}
-	xhr.send(null);
-}
+			var city = $('#ville').val();
+			var KeyAPI = '5fe735476d87c79b7f242f8f480d72a0';
+			var link = 'https://api.openweathermap.org/data/2.5/weather?q=' + city;
 
-function createXHR() 
-{
-    var request = false;
-        try {
-            request = new ActiveXObject('Msxml2.XMLHTTP');
-        }
-        catch (err2) {
-            try {
-                request = new ActiveXObject('Microsoft.XMLHTTP');
-            }
-            catch (err3) {
-		try {
-			request = new XMLHttpRequest();
-		}
-		catch (err1) 
-		{
-			request = false;
-		}
-            }
-        }
-    return request;
-}
+// Récupérer Le JSON qui correspond au lien.
+			$.getJSON(link + ',fr&lang=fr&units=metric&appid=' + KeyAPI)
+				.then(function(data){
+
+						var cityName = data.name;
+						console.log(cityName);
+						var coord = data.coord.lon;
+						console.log(coord);
+						var coord2 = data.coord.lat;
+						console.log(coord2);
+						var windspeed = data.wind.speed;
+						console.log(windspeed);
+						var hum = data.main.humidity;
+						console.log(hum);
+						var pressure = data.main.pressure;
+						console.log(pressure);
+						var tempMain = data.main.temp;
+						console.log(tempMain);
+						// var tempMin = main.temp_min;
+						// console.log(tempMin);
+						// var tempMax = main.temp_max;
+						// console.log(tempMax);
+
+					})
+			});
+
+
 })();
