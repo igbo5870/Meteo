696658be5b1e79481d9db9287b532a9a4d193bf5
christelletalbot <christelletalbot12@gmail.com>
2017-12-29 14:10:43 +0100

    map

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 (function main(){
 
 // Obtention data meteo
@@ -24,6 +24,7 @@
 		var longitude = data.coord.lon;
 		var vent = data.wind.speed;
 		var name = data.name;
+		var icon = data.weather;
 
 		$('.temp').html(temp + ' °C');
 		$('.pressure').html(pressure + ' hPa');
@@ -42,104 +43,17 @@
 			select.append('<option value=' + i + '>' + localStorage.getItem(i) + '</option>');
 			
 			}
-			 
+		function init(){
+		var mymap = L.map('mapid').setView([46.3630104, 2.9846608], 6);
+		//$('#mapid').html(mymap);	 
+		}
+		
 		});
 		//localStorage.clear();
-
+		//var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+		//$('#mapid').html(mymap);
 	
 	});
  
 })();
-=======
-	});
-	//localStorage.clear(); // vider le cache 
-});
 
-})();
-
-
-
-
-
-
-
-
-
-
-
-
-// (function main(){
-// var xhr=createXHR();	
-// $('#mybtn').on('click',function(){
-// 	//getJson();
-// 	getJqAjax();
-// })
-// function getJqAjax(){
-// 	var mylat = $('#mylat').val();
-// 	var mylng = $('#mylng').val();
-// 	$.ajax({
-// 		url:"https://www.prevision-meteo.ch/services/json/lat="+ mylat +"lng="+mylng+"",
-// 		datatype:'json',
-// 		success: function(data){
-// 			console.log("success");
-// 			console.log(data.fcst_day_0.condition)
-// 		},
-// 		error: function(){
-// 			console.log("no way, i'm fucked");
-// 		}
-// 	})
-// }
-// function getJson(){
-// 	var mylat = $('#mylat').val();
-// 	var mylng = $('#mylng').val();
-
-// 	var myurl = "https://www.prevision-meteo.ch/services/json/lat="+ mylat +"lng="+mylng;
-// 	//var myurl = "http://www.geobi.fr/dev/googleplace/places.php?lat="+ mylat +"&lon="+mylng+"&type=pharmacy&radius=10000";
-// 	//"http://www.geobi.fr/dev/googleplace/places.php?lat=44.334862&lon=2.435056&type=pharmacy&radius=10000"
-// 	var objJSON;
-	
-// 	xhr.open("GET", myurl,true);
-
-// 	xhr.onreadystatechange=function()
-// 	{
-				
-// 		if (xhr.readyState == 4) 
-// 		{
-// 			if (xhr.status != 404) 
-// 			{
-// 				var maMeteo=eval("(" + xhr.responseText + ")");
-// 				var myDiv = $('#meteo');
-// 				var contenu ="";
-// 				//var contenu = maMeteo.
-// 				var myCondition = maMeteo.fcst_day_0.condition;
-// 				alert(mycondition);
-// 			}
-// 		}
-// 	}
-// 	xhr.send(null);
-// }
-
-// function createXHR() 
-// {
-//     var request = false;
-//         try {
-//             request = new ActiveXObject('Msxml2.XMLHTTP');
-//         }
-//         catch (err2) {
-//             try {
-//                 request = new ActiveXObject('Microsoft.XMLHTTP');
-//             }
-//             catch (err3) {
-// 		try {
-// 			request = new XMLHttpRequest();
-// 		}
-// 		catch (err1) 
-// 		{
-// 			request = false;
-// 		}
-//             }
-//         }
-//     return request;
-// }
-// })();
->>>>>>> chris
